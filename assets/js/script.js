18df60c9d5bdcc91913fefba7ca929730bcf9538
Brett Fleming <brettfleming015@gmail.com>
2021-06-18 22:33:01 -0500

    finished the javascript for the most part just needs minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,123 @@
+const questions = [
+    {question: "The best soccer player of all time?",
+        answers: [ { text: "Messi", correct: true }, { text: "Neymar", correct: false }, { text: "Ronaldo", correct: false }, { text: "Pele", correct: false } ]
+    },
+    { question: "Which country has won the most World Cups?",
+        answers: [ { text: "Italy", correct: false }, { text: "Germany", correct: false }, { text: "Spain", correct: false }, { text: "Brazil", correct: true } ]
+    },
+    { question: "Which player has the most ballon d'or awards?",
+        answers: [ { text: "Ronaldinho", correct: false }, { text: "Johan Cruyff", correct: false }, { text: "Ronaldo", correct: false }, { text: "Messi", correct: true } ]
+    },
+    { question: "which team has won the most trebles?",
+        answers: [ { text: "F.C. Bayern Munich", correct: true }, { text: "FC Barcelona", correct: true }, { text: "Manchester United F.C", correct: false }, { text: "Inter Milan", correct: false } ]
+    },
+    { question: "Which team has won the most FA cups?",
+        answers: [ { text: "Liverpool", correct: false }, { text: "Chelsea", correct: false }, { text: "Manchester United", correct: false }, { text: "Arsenal", correct: true } ]
+    },
+    { question: "which goal keeper is the only the only one to win the ballon d'or?",
+        answers: [ { text: "Peter Schmeichel", correct: false }, { text: "Lev Yashin", correct: true }, { text: "Manuel Neuer", correct: false }, { text: "Gianluigi Buffon", correct: false } ]
+    },
+    { question: "How many Champions league wins to AC Milan have?",
+        answers: [ { text: "10", correct: false }, { text: "6", correct: false }, { text: "7", correct: true }, { text: "4", correct: false } ]
+    }
+]
+const  timerEl = document.getElementById("timer");
+const scoreEl = document.getElementById("score");
+const highscoreEl = document.getElementById("highscore");
 const startButton = document.getElementById("start_button");
-const questionEl = document.getElementById("question_container")
-startButton.addEventListener("click", startGame);
+const nextButton = document.getElementById("next_button");
 
 
+// question elements 
+const questionBoxEl = document.getElementById("question_container");
+const questionEl = document.getElementById("question");
+const answerEl = document.getElementById("answers");
 
-function startGame() {
-    startButton.classList.add("hide");
-    questionEl.classList.remove("hide");
+let currentScore = 0;
+let timeLeft = 120;
+
+
+let selectedQuestion 
+let currentQuestion
+let highscore = window.localStorage.getItem("highscore", highscore);
+scoreEl.innerText = "Score: " + currentScore;
+
+window.localStorage.getItem("highscore");
+
+function start() {
+    startButton.classList.add("hidden");
+    selectedQuestion = questions
+    currentQuestion = 0;
+    questionBoxEl.classList.remove("hidden");
+    startTimer();
     nextQuestion();
 }
-
+function startTimer() {
+   
+    var timer = setInterval(function(){
+    timerEl.innerHTML = "timer: " + timeLeft + " secs";
+    timeLeft --;
+    if (timeLeft < 0) {
+        clearInterval(timer);
+        gameOver();
+    }
+}, 1000);
+}
 
 function nextQuestion() {
+    reset() 
+    showQuestion(selectedQuestion[currentQuestion]);
+    
+}
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("button")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+        answerEl.appendChild(button)
+    });
+}
+
+    
+   
+function selectAnswer(event) {
+    const selectedButton = event.target
+    if (selectedButton.dataset.correct) {
+        currentScore = currentScore + 1;
+        scoreEl.innerText = "Score: " + currentScore;
+    }
+    if (selectedQuestion.length > currentQuestion + 1) {
+        nextButton.classList.remove("hidden") 
+    } else {
+        gameOver();
+    }
     
 }
 
-function selectAnswer() {
+function gameOver() {
+    if (currentScore > highscore) {
+        highscore = currentScore
+        window.localStorage.setItem("highscore", highscore);
+    }
     
 }
+
+function reset() {
+    nextButton.classList.add("hidden")
+    while (answerEl.firstChild) {
+        answerEl.removeChild(answerEl.firstChild)
+    }
+}
+    
+
+
+startButton.addEventListener("click", start);
+nextButton.addEventListener("click", () => {
+    currentQuestion++;
+    nextQuestion();
+});
